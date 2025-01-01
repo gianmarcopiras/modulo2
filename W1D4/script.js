e608ccd2e3e892c6a611ebd0d2b23f2e8a71edcf
gianmarcopiras <giammy1606@gmail.com>
2025-01-01 19:14:48 +0100

    Add files via upload

---
+++ W1D4/script.js
@@ -0,0 +1,40 @@
+/*ESERCIZIO 1
+Stringa: delimitata da doppi apici o singoli apici, generalmente contiene testo;
+numero: numero intero;
+booleano: dal matematico George Boole, pioniere della matematica logica, ovvero può avere valore true o false
+array: un array viene dichiarato usando le parentesi quadre, e può contenere un insieme di valori di qualsiasi tipo
+oggetto: un oggetto viene dichiarato con le parentesi graffe, e contiene coppie di CHIAVE-VALORE
+UNDEFINED: viene impiegato per rappresentare il fatto che una variabile non è stata dichiarata; quest'ultima, inoltre, potrebbe essere stata dichiarata ma non ancora assegnata ad un valore specifico;
+NULL: null è invece un'assegnazione di valore che significa "nessun valore".*/
+
+//ESERCIZIO 2
+let numeroUno = 12;
+let numeroDue = 20;
+let risultato= numeroUno + numeroDue;
+
+console.log("il risultato è", risultato);
+
+//ESERCIZIO 3   
+let x = 12
+
+//ESERCIZIO 4
+let name = "Gianmarco";
+
+//ESERCIZIO 5
+let differenza;
+console.log(differenza);
+
+//ESERCIZIO 6
+
+let Gianmarco = {
+    student: "Gianmarco",
+    surname: "Piras",
+    hobby: "cucinare"
+}
+
+//ESERCIZIO 7
+
+let name1 = "john";
+let name2 = "John";
+console.log(name1 === name2);
+console.log(name1.toLowerCase() === name2.toLowerCase());
